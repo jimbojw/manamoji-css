eb213a7a7e1afc4c8da0b34995e81f18ff23d727
jimbojw <wilson.jim.r@gmail.com>
2024-11-25 09:41:49 -0500

    build: Adding semantic-release for automated versioning, release notes etc.

---
+++ release.config.mjs
@@ -0,0 +1,53 @@
+/**
+ * @license SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * @fileoverview Configuration for semantic-release.
+ */
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Directory of this script, e.g. `__dirname`.
+const SCRIPT_DIR = path.dirname(fileURLToPath(import.meta.url));
+
+// Plugins to run in both dry run and production modes.
+const COMMON_PLUGINS = [
+  ["@semantic-release/commit-analyzer"],
+  ["@semantic-release/release-notes-generator"],
+  ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+];
+
+// Config to use when `--dry-run` is present in the process argv.
+const DRY_RUN_CONFIG = {
+  repositoryUrl: `file://${SCRIPT_DIR}`,
+  branches: ["*"],
+  plugins: COMMON_PLUGINS,
+};
+
+// Production config to run on CI (GitHub Actions).
+const PROD_CONFIG = {
+  branches: ["main"],
+  plugins: [
+    ...COMMON_PLUGINS,
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "CHANGELOG.md",
+          "dist/**",
+          "package.json",
+          "package-lock.json",
+        ],
+        message:
+          "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    ["@semantic-release/github"],
+  ],
+};
+
+// Export config matching presence of dry run flag.
+export default process.argv.includes("--dry-run")
+  ? DRY_RUN_CONFIG
+  : PROD_CONFIG;
